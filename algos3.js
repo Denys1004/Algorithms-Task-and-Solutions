5da87d8285c999e277a271d0e93f744ddca2f134
denys krychevskyi <dkrychevskyi@gmail.com>
2020-10-05 16:26:13 -0700

    added 4

--- algos3.js
+++ algos3.js
@@ -46,3 +46,13 @@ function swapPairs(arr) {
 		arr[i + 1] = temp;
 	}
 }
+
+// Swap positions of successive pairs of values of given array. If length is odd, do not change the final element. For [1,2,3,4], return [2,1,4,3]. For example, change input ["Brendan",true,42] to [true,"Brendan",42]. 
+// As with all array challenges, do this without using any built-in array methods.
+function swapPairs(arr) {
+	for(let i = 0; i < arr.length - 1; i = i + 2) {
+		let temp = arr[i];
+		arr[i] = arr[i + 1];
+		arr[i + 1] = temp;
+	}
+}
