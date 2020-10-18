171206b96c0047f12cb868ac9af95c02c1cd36da
denys krychevskyi <dkrychevskyi@gmail.com>
2020-10-18 10:48:17 -0700

    added9

--- algorithms.js
+++ algorithms.js
@@ -54,3 +54,20 @@ for(var i=0; i<5;i++){
  b = y(10);
  console.log(b);
  // 15, 10, 20
+
+ function minToFront(arr) {
+   var min = arr[0];
+   var idx = 0;
+   for (var i = 1; i < arr.length; i++){
+       if (arr[i] < min) {
+           min = arr[i];
+           idx = i;
+       }
+   }
+   for (var x = idx; x > 0; x--){
+       var temp = arr[x];
+       arr[x] = arr[x-1];
+       arr[x-1] = temp;
+   }
+   return arr;
+}
