85dc9dd714aa2233834e2daa4c25c0e851c63548
denys krychevskyi <dkrychevskyi@gmail.com>
2020-10-22 10:41:24 -0700

    addd 11

--- algorithms.js
+++ algorithms.js
@@ -89,3 +89,11 @@ function pushFront(arr, value) {
 	
 	arr[0] = value;
 }
+
+// Given an array, remove and return the value at the beginning of the array. Do this without using any built-in array methods except pop().
+function popFront(arr) {
+	const val = arr[0];
+	for(let i = 0; i < arr.length; i++)
+		arr[i] = arr[i + 1];
+	arr.length = arr.length - 1;
+}
