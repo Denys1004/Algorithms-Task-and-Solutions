c2e698640cb0419aff8720412e54fb1b97b43e21
denys krychevskyi <dkrychevskyi@gmail.com>
2020-10-21 20:22:45 -0700

    added 10

--- algorithms.js
+++ algorithms.js
@@ -80,3 +80,12 @@ function y(a){
 }
 b = y(10);
 console.log(b);
+
+// Given an array and an additional value, insert this value at the beginning of the array. Do this without using any built-in array methods.
+function pushFront(arr, value) {
+	// move all values in array over
+	for(let i = arr.length; i > 0; i--)
+		arr[i] = arr[i-1]
+	
+	arr[0] = value;
+}
