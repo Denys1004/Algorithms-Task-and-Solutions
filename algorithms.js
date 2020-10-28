ab46a90c739633dd6dd8953bd29cbc0af3fbebf8
denys krychevskyi <dkrychevskyi@gmail.com>
2020-10-27 18:31:48 -0700

    csjn

--- algorithms.js
+++ algorithms.js
@@ -97,3 +97,12 @@ function popFront(arr) {
 		arr[i] = arr[i + 1];
 	arr.length = arr.length - 1;
 }
+
+// Given an array, index, and additional value, insert the value into array at given index. Do this without using built-in array methods. 
+// You can think of pushFront(arr,val) as equivalent to insertAt(arr,0,val).
+function insertAt(arr, idx, val) {
+	for(let i = arr.length; i > idx; i--)
+		arr[i] = arr[i-1]
+	
+	arr[idx] = val;
+}
